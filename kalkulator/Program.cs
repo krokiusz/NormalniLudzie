19767aed407353c86adb6642156f29922391e68b
unknown <kroky@o2.pl>
2019-09-22 15:30:00 +0200

    Introduce menu to application

--- kalkulator/Program.cs
+++ kalkulator/Program.cs
@@ -10,6 +10,20 @@ namespace kalkulator
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("1. Dodawanie");
+                Console.WriteLine("2. Odejmowanie");
+                Console.WriteLine("3. Mnożenie");
+                Console.WriteLine("3. Dzielenie");
+                int nemuOption = Convert.ToInt32(Console.ReadLine());
+
+                if(nemuOption == 0)
+                {
+                    break;
+                }
+            }
         }
     }
-}
+    }
